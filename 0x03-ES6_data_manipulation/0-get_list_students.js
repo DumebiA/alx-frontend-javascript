e5bf7b5241cae4583c4a56a59b1d704ed0dbf48f
root <anyamkpa.ifeanyi@gmail.com>
2024-04-29 10:15:21 -0700

    100 on task 1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,5 +3,5 @@ export default function getListStudents() {
 		{id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		{id: 2, firstName: 'James', location: 'Columbia' },
 		{id: 5, firstName: 'Serena', location: 'San Francisco' }
-	]
+	];
 }
